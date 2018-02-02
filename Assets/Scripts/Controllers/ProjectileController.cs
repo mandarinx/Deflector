66a6e2c534b6a1f31af4833dcfaf5b8e921ebf59
mandarinx <mandarin@mandarin.no>
2018-02-02 08:03:10 +0100

    Reset the projectile tag on will despawn

--- Assets/Scripts/Controllers/ProjectileController.cs
+++ Assets/Scripts/Controllers/ProjectileController.cs
@@ -6,16 +6,18 @@ public class ProjectileController : MonoBehaviour {
     [SerializeField]
     private GameObject                 prefab;
     private GameObjectPool<Projectile> pool;
-    
+
     private void Awake() {
-        pool = new GameObjectPool<Projectile>(transform, prefab, 16, true);
+        pool = new GameObjectPool<Projectile>(transform, prefab, 16, true) {
+            OnWillSpawn = p => { p.tag = "Untagged"; }
+        };
         pool.Fill();
     }
 
     public void Despawn(GameObject projectile) {
         pool.Despawn(projectile.GetComponent<Projectile>());
     }
-    
+
     public void Spawn(Vector3 pos) {
         Projectile projectile;
         pool.Spawn(out projectile);
