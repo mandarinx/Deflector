b99131b0288ae73bc4f433be19bf6832ae78b9f8
mandarinx <mandarin@mandarin.no>
2018-02-16 21:22:18 +0100

    Rewrote the pixelart camera

---
+++ Assets/Scripts/Camera/PixelArtCamera.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace LunchGame01 {
+
+    public class PixelArtCamera : MonoBehaviour {
+
+        [SerializeField]
+        private Vector2Int    targetRes;
+        [SerializeField]
+        private Material      rtMaterial;
+
+        private RenderTexture rt;
+        private Camera        cam;
+
+        private void Awake() {
+            cam = GetComponent<Camera>();
+            rt = new RenderTexture(targetRes.x,
+                                   targetRes.y,
+                                   0,
+                                   RenderTextureFormat.ARGB32)
+            {
+                filterMode       = FilterMode.Point,
+                autoGenerateMips = false,
+                anisoLevel       = 0,
+                wrapMode         = TextureWrapMode.Clamp
+            };
+            cam.targetTexture = rt;
+        }
+
+        private void OnPreRender() {
+            cam.targetTexture = rt;
+        }
+
+        private void OnPostRender() {
+            cam.targetTexture = null;
+
+            int scale   = BestFitScale(targetRes);
+            int width   = targetRes.x * scale;
+            int height  = targetRes.y * scale;
+            int offsetX = Mathf.FloorToInt((Screen.width - width) * 0.5f);
+            int offsetY = Mathf.FloorToInt((Screen.height - height) * 0.5f);
+
+            rtMaterial.SetVector("_Pos", new Vector4(offsetX,
+                                                     offsetY,
+                                                     offsetX + width,
+                                                     offsetY + height));
+            rtMaterial.SetTexture("_MainTex", rt);
+
+            Graphics.Blit(rt, null, rtMaterial);
+        }
+
+        private static int BestFitScale(Vector2Int res) {
+            return Mathf.Min(
+                Mathf.FloorToInt(Mathf.Max((float) Screen.width / res.x,  1)),
+                Mathf.FloorToInt(Mathf.Max((float) Screen.height / res.y, 1))
+            );
+        }
+    }
+}
---
+++ Assets/Visuals/Shaders/ScreenSpaceTexture.shader
@@ -0,0 +1,57 @@
+﻿Shader "Unlit/Screen Space Texture"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Pos ("Screen pos", Vector) = (0,0,1,1)
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            // note: no SV_POSITION in this struct
+            struct v2f {
+                float2 uv : TEXCOORD0;
+            };
+
+            v2f vert (
+                float4 vertex : POSITION, // vertex position input
+                float2 uv : TEXCOORD0, // texture coordinate input
+                out float4 outpos : SV_POSITION // clip space position output
+                )
+            {
+                v2f o;
+                o.uv = uv;
+                outpos = UnityObjectToClipPos(vertex);
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float4 _Pos;
+
+            fixed4 frag (v2f i, UNITY_VPOS_TYPE screenPos : VPOS) : SV_Target
+            {
+                // screenPos.xy will contain pixel integer coordinates.
+                // clip HLSL instruction stops rendering a pixel if value is negative
+
+                clip(screenPos.x - _Pos.x);
+                clip(_Pos.z - screenPos.x);
+                clip(screenPos.y - _Pos.y);
+                clip(_Pos.w - screenPos.y);
+
+                return tex2D(_MainTex, float2(
+                    float((screenPos.x - _Pos.x) / (_Pos.z - _Pos.x)),
+                    float((screenPos.y - _Pos.y) / (_Pos.w - _Pos.y))
+                ));
+            }
+            ENDCG
+        }
+    }
+}
