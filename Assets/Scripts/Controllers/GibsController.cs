6dae8b926e5bc6b5c4ba2be316aa0cded6a7b6b3
mandarinx <mandarin@mandarin.no>
2018-03-05 07:42:44 +0100

    Gibs controller and cleanup of visuals

---
+++ Assets/Scripts/Controllers/GibsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace LunchGame01 {
+    public class GibsController : MonoBehaviour {
+
+        [SerializeField]
+        private ParticleSystem prefab;
+
+        private GameObjectPool<ParticleSystem> pool;
+
+        private void Awake() {
+            pool = new GameObjectPool<ParticleSystem>(parent: transform,
+                                                      prefab: prefab.gameObject,
+                                                      size:   2,
+                                                      grow:   false) {
+                OnSpawned = OnParticleSystemSpawned
+            };
+            pool.Fill();
+        }
+
+        [UsedImplicitly]
+        public void SpawnAt(Vector3 position) {
+            ParticleSystem psys;
+            pool.Spawn(out psys);
+            psys.transform.position = position;
+        }
+
+        [UsedImplicitly]
+        public void DespawnAll() {
+            pool.Reset();
+        }
+
+        private void OnParticleSystemSpawned(ParticleSystem instance) {
+            StartCoroutine(DespawnWhenDone(instance, pool));
+        }
+
+        private static IEnumerator DespawnWhenDone(ParticleSystem                 psys,
+                                                   GameObjectPool<ParticleSystem> psysPool) {
+            while (psys.isPlaying) {
+                yield return null;
+            }
+            psysPool.Despawn(psys);
+        }
+    }
+}
